5eedba52d52a0bf0308bcdb9aef14cf5c2862895
HariharanK16 <hariharankannan1621@gmail.com>
2021-02-18 12:58:06 +0530

    DesignedForm

--- FrontEnd/supplychaintracker/lib/screens/authenticate/register.dart
+++ FrontEnd/supplychaintracker/lib/screens/authenticate/register.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:supplychaintracker/screens/authenticate/animation/FadeAnimation.dart';
 import 'package:supplychaintracker/services/auth.dart';
 import 'package:supplychaintracker/shared/constants.dart';
 import 'package:supplychaintracker/shared/loading.dart';
@@ -23,85 +24,207 @@ class _RegisterState extends State<Register> {
     return loading
         ? Loading()
         : Scaffold(
+            resizeToAvoidBottomInset: true,
             backgroundColor: Colors.white,
             appBar: AppBar(
-              backgroundColor: Colors.indigoAccent[90],
-              elevation: 0.2,
-              title: Text('Sign up'),
-              actions: <Widget>[
-                FlatButton.icon(
-                  icon: Icon(Icons.person),
-                  label: Text('Sign in'),
-                  onPressed: () {
-                    widget.toggleView();
-                  },
+              elevation: 0,
+              brightness: Brightness.light,
+              backgroundColor: Colors.white,
+              leading: IconButton(
+                onPressed: () {
+                  widget.toggleView();
+                },
+                icon: Icon(
+                  Icons.arrow_back_ios,
+                  size: 20,
+                  color: Colors.black,
                 ),
-              ],
+              ),
             ),
-            body: Container(
-              padding: EdgeInsets.symmetric(horizontal: 20.0, vertical: 50.0),
-              child: Form(
-                key: _validateKey,
-                child: Column(
-                  children: <Widget>[
-                    SizedBox(
-                      height: 20.0,
-                    ),
-                    TextFormField(
-                      decoration: textField.copyWith(hintText: 'Email'),
-                      validator: (val) => val.isEmpty ? 'Enter an email' : null,
-                      onChanged: (val) {
-                        setState(() => email = val);
-                      },
-                    ),
-                    SizedBox(
-                      height: 20.0,
-                    ),
-                    TextFormField(
-                      decoration: textField.copyWith(hintText: 'Password'),
-                      validator: (val) => (val.length < 8 || val.length > 15)
-                          ? 'Password length is 8-15'
-                          : null,
-                      obscureText: true,
-                      onChanged: (val) {
-                        setState(() => pwd = val);
-                      },
-                    ),
-                    SizedBox(
-                      height: 20.0,
-                    ),
-                    RaisedButton(
-                      color: Colors.blue,
-                      child: Text(
-                        'Sign up',
-                        style: TextStyle(color: Colors.white),
+            body: SingleChildScrollView(
+              child: Container(
+                padding: EdgeInsets.symmetric(horizontal: 40),
+                height: MediaQuery.of(context).size.height - 50,
+                width: double.infinity,
+                child: Form(
+                  key: _validateKey,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: <Widget>[
+                      FadeAnimation(
+                        1.2,
+                        Container(
+                          width: 150,
+                          height: 150,
+                          decoration: BoxDecoration(
+                            shape: BoxShape.circle,
+                            image: DecorationImage(
+                                image: AssetImage('assets/illustration.png'),
+                                fit: BoxFit.fill),
+                          ),
+                        ),
                       ),
-                      onPressed: () async {
-                        //print(email + ' ' + pwd);
-                        if (_validateKey.currentState.validate()) {
-                          setState(() {
-                            loading = true;
-                          });
-                          dynamic result = await _auth
-                              .registerWithEmailAndPassword(email, pwd);
-                          if (result == null) {
-                            setState(() {
-                              error = 'Please enter a valid email ID';
-                              loading = false;
-                            });
-                          }
-                        }
-                      },
-                    ),
-                    SizedBox(height: 12.0),
-                    Text(
-                      error,
-                      style: TextStyle(color: Colors.red, fontSize: 14.0),
-                    )
-                  ],
+                      Column(
+                        children: <Widget>[
+                          FadeAnimation(
+                              1,
+                              Text(
+                                "Sign up",
+                                style: TextStyle(
+                                    fontSize: 30, fontWeight: FontWeight.bold),
+                              )),
+                          SizedBox(
+                            height: 20,
+                          ),
+                          FadeAnimation(
+                              1.2,
+                              Text(
+                                "Create an account",
+                                style: TextStyle(
+                                    fontSize: 15, color: Colors.grey[700]),
+                              )),
+                        ],
+                      ),
+                      Column(
+                        children: <Widget>[
+                          FadeAnimation(1.2, makeInputemail(label: "Email")),
+                          FadeAnimation(1.3, makeInputpwd(label: "Password")),
+                        ],
+                      ),
+                      FadeAnimation(
+                          1.5,
+                          Container(
+                            padding: EdgeInsets.only(top: 3, left: 3),
+                            decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(50),
+                                border: Border(
+                                  bottom: BorderSide(color: Colors.black),
+                                  top: BorderSide(color: Colors.black),
+                                  left: BorderSide(color: Colors.black),
+                                  right: BorderSide(color: Colors.black),
+                                )),
+                            child: MaterialButton(
+                              minWidth: double.infinity,
+                              height: 60,
+                              onPressed: () async {
+                                print(email + ' ' + pwd);
+                                if (_validateKey.currentState.validate()) {
+                                  setState(() {
+                                    loading = true;
+                                  });
+                                  dynamic result = await _auth
+                                      .registerWithEmailAndPassword(email, pwd);
+                                  if (result == null) {
+                                    setState(() {
+                                      error = 'Please enter a valid email ID';
+                                      loading = false;
+                                    });
+                                  }
+                                }
+                              },
+                              color: Colors.greenAccent,
+                              elevation: 0,
+                              shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(50)),
+                              child: Text(
+                                "Sign up",
+                                style: TextStyle(
+                                    fontWeight: FontWeight.w600, fontSize: 18),
+                              ),
+                            ),
+                          )),
+                      FadeAnimation(
+                          1.6,
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: <Widget>[
+                              // Text("Already have an account?"),
+                              FlatButton(
+                                onPressed: () {
+                                  /*...*/
+                                  widget.toggleView();
+                                },
+                                child: Text(
+                                  "Already have an account? Login",
+                                  style: TextStyle(
+                                      fontWeight: FontWeight.normal,
+                                      fontSize: 18),
+                                ),
+                              ),
+                            ],
+                          )),
+                    ],
+                  ),
                 ),
               ),
             ),
           );
+  }
+
+  Widget makeInputemail({label}) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        Text(
+          label,
+          style: TextStyle(
+              fontSize: 15, fontWeight: FontWeight.w400, color: Colors.black87),
+        ),
+        SizedBox(
+          height: 5,
+        ),
+        TextFormField(
+          validator: (val) => val.isEmpty ? 'Enter an email' : null,
+          decoration: InputDecoration(
+            contentPadding: EdgeInsets.symmetric(vertical: 0, horizontal: 10),
+            enabledBorder: OutlineInputBorder(
+                borderSide: BorderSide(color: Colors.grey[400])),
+            border: OutlineInputBorder(
+                borderSide: BorderSide(color: Colors.grey[400])),
+          ),
+          onChanged: (val) {
+            setState(() => email = val);
+          },
+        ),
+        SizedBox(
+          height: 30,
+        ),
+      ],
+    );
+  }
+
+  Widget makeInputpwd({label}) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        Text(
+          label,
+          style: TextStyle(
+              fontSize: 15, fontWeight: FontWeight.w400, color: Colors.black87),
+        ),
+        SizedBox(
+          height: 5,
+        ),
+        TextFormField(
+          validator: (val) => (val.length < 8 || val.length > 15)
+              ? 'Password length is 8-15'
+              : null,
+          decoration: InputDecoration(
+            contentPadding: EdgeInsets.symmetric(vertical: 0, horizontal: 10),
+            enabledBorder: OutlineInputBorder(
+                borderSide: BorderSide(color: Colors.grey[400])),
+            border: OutlineInputBorder(
+                borderSide: BorderSide(color: Colors.grey[400])),
+          ),
+          obscureText: true,
+          onChanged: (val) {
+            setState(() => pwd = val);
+          },
+        ),
+        SizedBox(
+          height: 30,
+        ),
+      ],
+    );
   }
 }
